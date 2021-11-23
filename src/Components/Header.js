0a28d1ef7caf1660ee72c9d16619a36fbc78a279
ian <ianvinicioss@gmail.com>
2021-11-23 16:19:34 -0300

    Create context for user and initial login

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Styles from "./Header.module.css";
 import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from '../UserContext';
 
 function Header() {
+
+const {data} = React.useContext(UserContext);
+
   return (
     <header className={Styles.header}>
       <nav className={`${Styles.nav} container`}>
         <Link to="/" className={Styles.logo} aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login" className={Styles.login}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link to="/conta" className={Styles.login}>
+           {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={Styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
