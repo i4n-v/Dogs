4ae2d346a5ab0e152ca261bca8dc7ece78752c2a
ian <ianvinicioss@gmail.com>
2021-11-22 23:04:04 -0300

    Create routes to login and initial form

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
-import Login from "./Components/Login/Login"
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Styles from './Header.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 function Header() {
   return (
-    <div className={Styles.header}>
-      <nav className="container">
-        <Link to="/" end>Home</Link>
-        <Link to="/login">Login</Link>
+    <header className={Styles.header}>
+      <nav className={`${Styles.nav} container`}>
+        <Link to="/" className={Styles.logo} aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={Styles.login}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
-  )
+    </header>
+  );
 }
 
 export default Header;
