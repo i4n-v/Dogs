9e4c37646f08dbae75286162ff2ba3273847e2d0
ian <ianvinicioss@gmail.com>
2021-11-27 15:56:09 -0300

    Modifying the login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,20 +5,23 @@ import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import { UserContext } from "../../UserContext";
+import Styles from "./Login.module.css";
 
 function Login() {
   const { login } = useContext(UserContext);
 
   if(login) return <Navigate to="/conta" />
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={Styles.login}>
+        <div className={Styles.forms}>
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="criar" element={<LoginCreate />} />
+            <Route path="perdeu" element={<LoginPasswordLost />} />
+            <Route path="resetar" element={<LoginPasswordReset />} />
+          </Routes>
+        </div>
+    </section>
   );
 }
 
