8fa21c12e7b3387474ba2c61987af84857ca4f73
ian <ianvinicioss@gmail.com>
2021-11-28 12:52:12 -0300

    Create hook useFecth and protected route

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import Error from "../Helper/Error";
 import useForm from "../../Hooks/useForm";
 import { USER_POST } from "../../Api";
 import { UserContext } from "../../UserContext";
+import useFecth from "../../Hooks/useFecth";
 
 function LoginCreate() {
   const username = useForm();
@@ -11,6 +13,7 @@ function LoginCreate() {
   const password = useForm("password");
 
   const { userLogin } = React.useContext(UserContext);
+  const { error, loading, request } = useFecth();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,7 +22,7 @@ function LoginCreate() {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -30,7 +33,12 @@ function LoginCreate() {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="E-mail" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
