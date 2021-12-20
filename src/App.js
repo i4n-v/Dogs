b9856190c403ad8e27669e2404909215e57ec69b
ian <ianvinicioss@gmail.com>
2021-12-20 14:56:18 -0300

    Creating footer to the application

--- src/App.js
+++ src/App.js
@@ -14,18 +14,20 @@ import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
-            <ProtectedRoute path="foto/:id" element={<Photo />} />
-            <ProtectedRoute path="perfil/:user" element={<UserProfile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <ProtectedRoute path="conta/*" element={<User />} />
+              <ProtectedRoute path="foto/:id" element={<Photo />} />
+              <ProtectedRoute path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Styles from './Footer.module.css';
+import {ReactComponent as Dogs} from '../Assets/dogs-footer.svg';
 
 function Footer() {
   return (
-    <div className={Styles.footer}>
-      Footer
-    </div>
+    <footer className={Styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
   )
 }
 
