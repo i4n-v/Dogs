f952874e1ed1127565cac8c8510790ee2956d659
ian <ianvinicioss@gmail.com>
2021-12-20 14:23:25 -0300

    Adding reset password feature

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFecth from "../../Hooks/useFetch";
+import { PASSSWORD_LOST } from "../../Api";
+import Error from "../Helper/Error";
 
 function LoginPasswordLost() {
+  const login = useForm();
+  const { data, error, loading, request } = useFecth();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const urlReset = window.location.href.replace("perdeu", "resetar");
+      const { url, options } = PASSSWORD_LOST({
+        login: login.value,
+        url: urlReset,
+      });
+      request(url, options);
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
 }
 
 export default LoginPasswordLost;
--- src/api.js
+++ src/api.js
@@ -109,3 +109,29 @@ export function PHOTO_DELETE(id) {
     }
   }
 }
+
+export function PASSSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  }
+}
+
+export function PASSSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  }
+}
